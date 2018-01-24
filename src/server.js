bc9a56d8fb2a15b01e4566cd10fd5aa66c288446
James McQuaid <jpm5399@rit.edu>
2018-01-24 10:48:39 -0500

    Fixed small issue

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const htmlHandler = require('./htmlResponses.js');
 const textHandler = require('./textResponses.js');
 const jsonHandler = require('./jsonResponses.js');
-const imageHander = require('./imageResponses.js');
+const imageHandler = require('./imageResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -27,6 +27,9 @@ const onRequest = (request, response) => {
       break;
     case '/timeJSON':
       jsonHandler.getTimeJSON(request, response);
+      break;
+    case '/dankmeme':
+      imageHandler.getDankMeme(request, response);
       break;
     default:
       htmlHandler.getIndex(request, response);
